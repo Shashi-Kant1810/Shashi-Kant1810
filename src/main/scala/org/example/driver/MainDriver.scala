b5f9ce72eeeb56dc29a67720343e213710b0214b
Shashi-Kant1810 <shashithakur1810@gmail.com>
2024-06-23 13:40:59 +0530

    minor change in driver file
    minor change in driver file

--- src/main/scala/org/example/driver/MainDriver.scala
+++ src/main/scala/org/example/driver/MainDriver.scala
@@ -6,6 +6,7 @@ object MainDriver extends App with AppConfig{
     println("welcome to scala")
     val sampleData = Seq((1, "spark language","A","shop property"), (2, "Java","B","home"), (3, "Pyspark","C","hotel"))
     val df1 = spark.createDataFrame(sampleData).toDF("course_id", "course_name","property_type","property_used_for")
+    print("schema output")
     df1.printSchema()
 
 }
